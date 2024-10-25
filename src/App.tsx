9b7aa38dd96b656886af56d0d43fc3fd98baedd8
Jose Pedro Silva <josepedro88@live.com.pt>
2024-10-25 20:26:44 +0200

    implement routing with react-router-dom and add Dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Dashboard from './Components/Dashboard/'
+import Layout from './Components/Layout'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Dashboard />
+  }
+])
 
 function App() {
 
   return (
     <>
-      <p>Cleaned</p>
+      <Layout>
+        <RouterProvider router={router} />
+      </Layout>
     </>
   )
 }
 
 export default App
+
