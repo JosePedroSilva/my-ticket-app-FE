47745f5b844e06a59cd008ed78e470abec46dfb4
Jose Pedro Silva <josepedro88@live.com.pt>
2024-10-26 20:07:19 +0200

    refactor login component: update layout and styles, implement new CSS module

--- src/Components/Authentication/Login/index.tsx
+++ src/Components/Authentication/Login/index.tsx
@@ -1,27 +1,39 @@
-import { useState } from 'react';
-import useAuth from '../../../hooks/useAuth';
+// import { useState } from 'react';
+// import useAuth from '../../../hooks/useAuth';
+import styles from './styles.module.css'
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const { login } = useAuth();
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
+    // const { login } = useAuth();
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        try {
-            await login(email, password);
-            alert('Logged in successfully');
-        } catch (err) {
-            console.error(err);
-        }
-    };
+    // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    //     e.preventDefault();
+    //     try {
+    //         await login(email, password);
+    //         alert('Logged in successfully');
+    //     } catch (err) {
+    //         console.error(err);
+    //     }
+    // };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-            <button type="submit">Login</button>
-        </form>
+        <div className={styles.logincontainer}>
+            <div className={styles.loginbox}>
+                <h2>Login</h2>
+                <form>
+                <label >Email:</label>
+                <input type="email" id="priority" name="priority" />
+
+                <label >Password:</label>
+                <input type="password" id="start-date" name="start-date" />
+
+                <button type="submit">Submit</button>
+                <a href="#" className={styles.createaccount}>Create account</a>
+                </form>
+            </div>
+        </div>
+
     );
 };
 
