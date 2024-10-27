fdab5289326e628b8b376693ef4954011d9e4457
Jose Pedro Silva <josepedro88@live.com.pt>
2024-10-27 15:41:17 +0100

    feat(sidebar): added sidebar nav elements and style

--- src/Components/SideBar/index.tsx
+++ src/Components/SideBar/index.tsx
@@ -7,8 +7,33 @@ interface SideBarProps {
 const SideBar: React.FC<SideBarProps> = ({children}) => {
     return (
         <div className={styles.fullLayout}>  
-            <div className={styles.sideBarContainer}>
-                <p>Side bar</p>
+            <div className={`${styles.sideBarBG}`}>
+                <div className={`${styles.sideBarContainer} content-margin-large `}>
+                    <h1>Ticket app</h1>
+
+                    <div className={styles.currentProjectsContainer}>
+                        <p>Current Project:</p>
+                        <p>Project Name // No Project</p>
+                    </div>
+
+                    <nav>
+                        <ul>
+                            <li className={styles.active}>Dashboard</li>
+                            <li>Projects</li>
+                            <li>List</li>
+                            <li>Timer</li>
+                            <li>Settings</li>
+                        </ul>
+                    </nav>
+
+                    <div className={styles.recentEventsContainer}>
+                        <p>Recent events:</p>
+                        <ul>
+                            <li>Ticket created by x: Do something</li>
+                            <li>Ticket created by x: Do something</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
             <div className={styles.contentWindowContainer}>{children}</div>
         </div>
