6ece50c4bef68471e2ad1c9e8dd5c1484121d969
Jose Pedro Silva <josepedro88@live.com.pt>
2024-10-28 14:20:52 +0100

    refactor: moved ProjectTaskCreator to correct components folder

--- src/Components/layouts/DashboardLayouts.tsx
+++ src/Components/layouts/DashboardLayouts.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import SideBar from '../SideBar';
-import ProjectTaskCreator from '../../ProjectTaskCreator';
+import ProjectTaskCreator from '../ProjectTaskCreator';
 
 const DashboardLayout = () => {
     return (
